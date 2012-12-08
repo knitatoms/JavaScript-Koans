260167749c2897a9d2b5e2ce38b3d9b233af93a3
Tom Atkins <knitatoms@gmail.com>
2012-12-08 08:25:07 +0000

    2nd commit now I'm about a third of the way through

--- topics/about_strings.js
+++ topics/about_strings.js
@@ -4,18 +4,18 @@ module("About Strings (topics/about_strings.js)");
 test("delimiters", function() {
     var singleQuotedString = 'apple';
     var doubleQuotedString = "apple";
-    equals(singleQuotedString === doubleQuotedString, __, 'are the two strings equal?');
+    equals(singleQuotedString === doubleQuotedString, true, 'are the two strings equal?');
 });
 
 test("concatenation", function() {
     var fruit = "apple";
     var dish = "pie";
-    equals(fruit + " " + dish, __, 'what is the value of fruit + " " + dish?');
+    equals(fruit + " " + dish, "apple pie", 'what is the value of fruit + " " + dish?');
 });
 
 test("character Type", function() {
     var characterType = typeof("Amory".charAt(1)); // typeof will be explained in about reflection
-    equals(characterType, __, 'Javascript has no character type');
+    equals(characterType, "string", 'Javascript has no character type');
 });
 
 test("escape character", function() {
